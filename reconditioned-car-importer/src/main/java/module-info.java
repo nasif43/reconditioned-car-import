fc1fd2d2ac83a9436cf863c6e21f9d185d0f4951
Muhtasim <muhtasimhossain43@gmail.com>
2025-04-12 16:31:35 +0600

    first commit

---
+++ reconditioned-car-importer/src/main/java/module-info.java
@@ -0,0 +1,8 @@
+module cse213.reconditionedcarimporter {
+    requires javafx.controls;
+    requires javafx.fxml;
+
+
+    opens cse213.reconditionedcarimporter to javafx.fxml;
+    exports cse213.reconditionedcarimporter;
+}
