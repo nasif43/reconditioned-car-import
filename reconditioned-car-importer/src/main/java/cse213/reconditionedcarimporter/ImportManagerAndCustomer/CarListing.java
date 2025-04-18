2b3b88979446716a7e6b2a8cf8a7846587a8982c
asifahmed6969 <asifahmed0545@gmail.com>
2025-04-18 22:39:40 +0600

    so i have completed the new classes like carlisting ,invoice, PurchaseRequest

--- reconditioned-car-importer/src/main/java/cse213/reconditionedcarimporter/ImportManagerAndCustomer/CarListing.java
+++ reconditioned-car-importer/src/main/java/cse213/reconditionedcarimporter/ImportManagerAndCustomer/CarListing.java
@@ -3,5 +3,69 @@ package cse213.reconditionedcarimporter.ImportManagerAndCustomer;
 import java.util.ArrayList;
 
 public class CarListing {
+
+    private String listingId;                  // Unique ID for this listing
+    private String title;                      // e.g., "New Arrival: Japanese Cars"
+    private String description;                // Summary about the listing
+    private String createdBy;                  // Name or ID of the Import Manager
+    private String createdDate;                // When the listing was created
     public ArrayList<Vehicle> carList;
+
+    public String getListingId() {
+        return listingId;
+    }
+
+    public void setListingId(String listingId) {
+        this.listingId = listingId;
+    }
+
+    public String getTitle() {
+        return title;
+    }
+
+    public void setTitle(String title) {
+        this.title = title;
+    }
+
+    public String getDescription() {
+        return description;
+    }
+
+    public void setDescription(String description) {
+        this.description = description;
+    }
+
+    public String getCreatedBy() {
+        return createdBy;
+    }
+
+    public void setCreatedBy(String createdBy) {
+        this.createdBy = createdBy;
+    }
+
+    public String getCreatedDate() {
+        return createdDate;
+    }
+
+    public void setCreatedDate(String createdDate) {
+        this.createdDate = createdDate;
+    }
+
+    public ArrayList<Vehicle> getCarList() {
+        return carList;
+    }
+
+    public void setCarList(ArrayList<Vehicle> carList) {
+        this.carList = carList;
+    }
+
+    public CarListing(String listingId, String title, String description, String createdBy, String createdDate, ArrayList<Vehicle> carList) {
+        this.listingId = listingId;
+        this.title = title;
+        this.description = description;
+        this.createdBy = createdBy;
+        this.createdDate = createdDate;
+        this.carList = carList;
+
+    }
 }
--- reconditioned-car-importer/src/main/java/cse213/reconditionedcarimporter/ImportManagerAndCustomer/PurchaseRequest.java
+++ reconditioned-car-importer/src/main/java/cse213/reconditionedcarimporter/ImportManagerAndCustomer/PurchaseRequest.java
@@ -1,4 +1,68 @@
 package cse213.reconditionedcarimporter.ImportManagerAndCustomer;
 
 public class PurchaseRequest {
+    private String requestId;
+    private String customerId;
+    private String vehicleId;
+    private float maxBidAmount;
+    private String status;
+    private String requestDate;
+
+    public String getRequestId() {
+        return requestId;
+    }
+
+    public void setRequestId(String requestId) {
+        this.requestId = requestId;
+    }
+
+    public String getCustomerId() {
+        return customerId;
+    }
+
+    public void setCustomerId(String customerId) {
+        this.customerId = customerId;
+    }
+
+    public String getVehicleId() {
+        return vehicleId;
+    }
+
+    public void setVehicleId(String vehicleId) {
+        this.vehicleId = vehicleId;
+    }
+
+    public float getMaxBidAmount() {
+        return maxBidAmount;
+    }
+
+    public void setMaxBidAmount(float maxBidAmount) {
+        this.maxBidAmount = maxBidAmount;
+    }
+
+    public String getStatus() {
+        return status;
+    }
+
+    public void setStatus(String status) {
+        this.status = status;
+    }
+
+    public String getRequestDate() {
+        return requestDate;
+    }
+
+    public void setRequestDate(String requestDate) {
+        this.requestDate = requestDate;
+    }
+
+    public PurchaseRequest(String requestId, String customerId, String vehicleId, float maxBidAmount, String status, String requestDate) {
+        this.requestId = requestId;
+        this.customerId = customerId;
+        this.vehicleId = vehicleId;
+        this.maxBidAmount = maxBidAmount;
+        this.status = status;
+        this.requestDate = requestDate;
+
+    }
 }
