dab562663d51d22aeae5cef4cfdd809952f393fa
asifahmed6969 <asifahmed0545@gmail.com>
2025-04-19 02:17:59 +0600

    checking

---
+++ reconditioned-car-importer/src/main/java/cse213/reconditionedcarimporter/ImportManagerAndCustomer/dummy.java
@@ -0,0 +1,18 @@
+package cse213.reconditionedcarimporter.ImportManagerAndCustomer;
+
+public class dummy {
+    private String name;
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public dummy(String name) {
+        this.name = name;
+
+    }
+}
