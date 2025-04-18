82120edf115f7d9f0f73ccb67c6dfc6bbee4198e
Nazia <naziaputul.nop@gmail.com>
2025-04-18 16:38:53 +0600

    fixed warehouse class

--- reconditioned-car-importer/src/main/java/cse213/reconditionedcarimporter/InventoryAndQuality/ShowroomSlot.java
+++ reconditioned-car-importer/src/main/java/cse213/reconditionedcarimporter/InventoryAndQuality/ShowroomSlot.java
@@ -1,6 +1,8 @@
 package cse213.reconditionedcarimporter.InventoryAndQuality;
 
-public class ShowroomSlot {
+import java.io.Serializable;
+
+public class ShowroomSlot extends Serializable {
 private int slotId,displayPriority,row,column;
 private String slotLabel;
 private boolean isOccupied;
--- reconditioned-car-importer/src/main/java/cse213/reconditionedcarimporter/InventoryAndQuality/Warehouse.java
+++ reconditioned-car-importer/src/main/java/cse213/reconditionedcarimporter/InventoryAndQuality/Warehouse.java
@@ -1,15 +1,16 @@
 package cse213.reconditionedcarimporter.InventoryAndQuality;
 
-import java.io.Serializable;
-import java.util.ArrayList;
+public abstract class Warehouse extends Location{
+    private WarehouseType type ;
+    private int capacity;
+    private ArrayList<Vehicle> vehicles;
 
-public class Warehouse extends Location implements Serializable {
-private WarehouseType type ;
-private int capacity;
-private ArrayList<Vehicle> vehicles;
-   //methods
-    public boolean isFull(){return false;};
-    public float calculateUtilization(){return 0;} ;
+    public boolean isFull(){
+        return false;
+    }
+    public void calculateutilization(){
+        return vehicles.size().equals(capacity);
+    }
     public void addShipment(Shipment shipment){
         ArrayList<Vehicle> vehiclesFromShipment = shipment.getVehicles();
         vehicles.addAll(vehiclesFromShipment);
