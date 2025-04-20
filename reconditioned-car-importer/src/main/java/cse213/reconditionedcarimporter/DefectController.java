3542068d15d05ae091396126568d831c4309fd47
DarthNihiluss <sadaf42069@gmail.com>
2025-04-21 03:29:25 +0600

    did some fxml shit

--- reconditioned-car-importer/src/main/java/cse213/reconditionedcarimporter/DefectController.java
+++ reconditioned-car-importer/src/main/java/cse213/reconditionedcarimporter/DefectController.java
@@ -1,11 +1,30 @@
 package cse213.reconditionedcarimporter;
 
 import javafx.scene.control.ComboBox;
+import javafx.scene.control.RadioButton;
+import javafx.scene.control.TableColumn;
+import javafx.scene.control.ToggleGroup;
 
 public class DefectController
 {
     @javafx.fxml.FXML
     private ComboBox <String> DefectSeverityComboBox;
+    @javafx.fxml.FXML
+    private RadioButton NoRadioButton;
+    @javafx.fxml.FXML
+    private RadioButton YesRadioButton;
+    @javafx.fxml.FXML
+    private ToggleGroup Approval;
+    @javafx.fxml.FXML
+    private TableColumn DefectTableview;
+    @javafx.fxml.FXML
+    private TableColumn ApprovalTableview;
+    @javafx.fxml.FXML
+    private TableColumn CostTableview;
+    @javafx.fxml.FXML
+    private TableColumn CategoryTableview;
+    @javafx.fxml.FXML
+    private TableColumn SeverityTableview;
 
     @javafx.fxml.FXML
     public void initialize() {
--- reconditioned-car-importer/src/main/java/cse213/reconditionedcarimporter/EquipmentController.java
+++ reconditioned-car-importer/src/main/java/cse213/reconditionedcarimporter/EquipmentController.java
@@ -1,7 +1,20 @@
 package cse213.reconditionedcarimporter;
 
+import javafx.scene.control.*;
+
 public class EquipmentController
 {
+    @javafx.fxml.FXML
+    private ComboBox DefectSeverityComboBox;
+    @javafx.fxml.FXML
+    private TextField NameFXID;
+    @javafx.fxml.FXML
+    private TextField TypeFXID;
+    @javafx.fxml.FXML
+    private DatePicker Datepicking;
+    @javafx.fxml.FXML
+    private TextField EquipmentIDFXID;
+
     @javafx.fxml.FXML
     public void initialize() {
     }}
