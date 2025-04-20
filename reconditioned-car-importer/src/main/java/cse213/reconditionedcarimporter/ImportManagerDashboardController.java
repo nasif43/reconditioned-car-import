df9cbe82ea5ad2f07a3fcdb9929b2276926f6713
asifahmed6969 <asifahmed0545@gmail.com>
2025-04-21 03:30:57 +0600

    idk whgat i do but i did it works almost done with my dashboards gg

---
+++ reconditioned-car-importer/src/main/java/cse213/reconditionedcarimporter/ImportManagerDashboardController.java
@@ -0,0 +1,38 @@
+package cse213.reconditionedcarimporter;
+
+import javafx.event.ActionEvent;
+
+public class ImportManagerDashboardController
+{
+    @javafx.fxml.FXML
+    public void initialize() {
+    }
+
+    @Deprecated
+    public void LogoutOnAction(ActionEvent actionEvent) {
+    }
+
+    @Deprecated
+    public void ApprovePaymentsOnACtion(ActionEvent actionEvent) {
+    }
+
+    @Deprecated
+    public void PurchaseRequestOnACtion(ActionEvent actionEvent) {
+    }
+
+    @Deprecated
+    public void CarListingOnACtion(ActionEvent actionEvent) {
+    }
+
+    @Deprecated
+    public void HomeOnAction(ActionEvent actionEvent) {
+    }
+
+    @Deprecated
+    public void ShipmentOnAction(ActionEvent actionEvent) {
+    }
+
+    @Deprecated
+    public void SupplierOrdersOnACtion(ActionEvent actionEvent) {
+    }
+}
