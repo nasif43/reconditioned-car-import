6d726d5623779339016cb10ae880e1c74527809c
Nazia <naziaputul.nop@gmail.com>
2025-04-20 22:36:01 +0600

    added flagagingview and assignvehicle to showrrom view

---
+++ reconditioned-car-importer/src/main/java/cse213/reconditionedcarimporter/InventoryAndQuality/AssignVehiclesToShowroomViewController.java
@@ -0,0 +1,7 @@
+package cse213.reconditionedcarimporter.InventoryAndQuality;
+
+public class AssignVehiclesToShowroomViewController
+{
+    @javafx.fxml.FXML
+    public void initialize() {
+    }}
--- reconditioned-car-importer/src/main/java/cse213/reconditionedcarimporter/InventoryAndQuality/ShowroomSlot.java
+++ reconditioned-car-importer/src/main/java/cse213/reconditionedcarimporter/InventoryAndQuality/ShowroomSlot.java
@@ -6,16 +6,16 @@ import java.io.Serializable;
 
 public class ShowroomSlot implements Serializable {
 private int slotId,displayPriority,row,column;
-private String slotLabel;
+private String slotName;
 private boolean isOccupied;
 private Vehicle assignedVehicle;
 
-    public ShowroomSlot(int slotId, int displayPriority, int row, int column, String slotLabel, boolean isOccupied, Vehicle assignedVehicle) {
+    public ShowroomSlot(int slotId, int displayPriority, int row, int column, String slotName, boolean isOccupied, Vehicle assignedVehicle) {
         this.slotId = slotId;
         this.displayPriority = displayPriority;
         this.row = row;
         this.column = column;
-        this.slotLabel = slotLabel;
+        this.slotName = slotName;
         this.isOccupied = isOccupied;
         this.assignedVehicle = assignedVehicle;
     }
@@ -60,11 +60,11 @@ public boolean isReserved(){return false;};
     }
 
     public String getSlotLabel() {
-        return slotLabel;
+        return slotName;
     }
 
-    public void setSlotLabel(String slotLabel) {
-        this.slotLabel = slotLabel;
+    public void setSlotLabel(String slotName) {
+        this.slotName = slotName;
     }
 
     public boolean isOccupied() {
